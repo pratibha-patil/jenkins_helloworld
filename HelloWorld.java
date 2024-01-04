fc483e0f7b1fc1fae7ea42c95074e37ea204374b
pratibha-patil <pratibha.patil.2022@gmail.com>
2024-01-04 13:47:10 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("trying webhook for helloworld");
+        System.out.println("trying webhook for helloworld from jenkins");
     }
 }
